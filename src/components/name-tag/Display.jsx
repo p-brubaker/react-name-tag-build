2c7e85bf0389db67bd7dd51f51deebddb50c0f94
Paul Brubaker <pbrubak@gmail.com>
2021-11-05 15:55:57 -0700

    tests passing

--- src/components/name-tag/Display.jsx
+++ src/components/name-tag/Display.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-const Display = () => ();
+function Display({ name, greeting }) {
+  return <h1>{`${greeting} ${name}`}</h1>;
+}
 
-Display.propTypes = {};
+Display.propTypes = {
+  name: PropTypes.string.isRequired,
+  greeting: PropTypes.string.isRequired,
+};
 
 export default Display;
